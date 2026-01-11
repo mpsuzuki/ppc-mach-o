38a4e9a92f7386171b023a06205854ad1a8b7bea
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2026-01-11 10:52:45 +0900

    embed absolute path of resolvepath in wrappers

--- triplet-cctool-skel.sh
+++ triplet-cctool-skel.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-if ! command -v resolvepath >/dev/null 2>&1
+RESOLVEPATH=@RESOLVEPATH@
+if test -x "${RESOLVEPATH}"
 then
+  :
+elif command -v resolvepath >/dev/null 2>&1
+then
+  RESOLVEPATH=resolvepath
+else
   echo "This wrapper script requires 'resolvepath' command" 1>&2
   exit 1
 fi
 
-path_me=`resolvepath $0`
+path_me=`${RESOLVEPATH} $0`
 dir_bin=`dirname "${path_me}"`
 dir_prefix=`dirname "${dir_bin}"`
 dir_libexec="${dir_prefix}"/libexec/cctools
